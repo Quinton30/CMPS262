d5b1e2c4fef5c793b128a0a2d7751d7f18ae8b09
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-03-26 20:05:57 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ require('dotenv').config(); // Load environment variables
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'students_fqe5_user',
-    host: 'cvi01adrie7s73ebi45g-a.oregon-postgres.render.com',
-    database: 'students_fqe5',
-    password: 'Rqur05qinOVXKi1EEbi3wE8XOAHczozY',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false,
     }
