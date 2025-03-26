2f7b7152876774092c5a4f29dfdf84426c0a166e
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-03-26 15:54:40 +0300

    The cool files

---
+++ db.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // Load environment variables
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false, // Required for Render PostgreSQL
+    }
+});
+
+module.exports = pool;
