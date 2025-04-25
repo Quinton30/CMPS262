c8f6f31e737e26049bd746b2c42a1627f7dd9a5a
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-04-25 17:36:34 +0300

    new Database

--- Module3/db.js
+++ Module3/db.js
@@ -10,7 +10,7 @@ const { Pool } = require('pg');
     port: 5432,
 });*/
 
-const pool = new Pool({
+/*const pool = new Pool({
     user: 'students_fqe5_user',
     host: 'cvi01adrie7s73ebi45g-a.oregon-postgres.render.com',
     database: 'students_fqe5',
@@ -19,6 +19,16 @@ const pool = new Pool({
     ssl: {
         rejectUnauthorized: false,
     }
-});
+}); Expired Database */
+const pool = new Pool({
+  user: 'postgres.auqsxkehosbvhviedypm',
+  host: 'aws-0-us-east-2.pooler.supabase.com',
+  database: 'postgres',
+  password: '#5@w$MM5SdSG8CK',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
 
 module.exports = pool;
